04929c675d7c43417e3a22f72f7d08dfd578cb31
Simone Amadio <amadiosimone1996@gmail.com>
2019-12-20 12:14:10 +0100

    First test solved

--- src/test/java/iotest/DotsAndBoxesInputTest.java
+++ src/test/java/iotest/DotsAndBoxesInputTest.java
@@ -4,13 +4,23 @@ package iotest;
 import dotsandboxes.DotsAndBoxesInput;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DotsAndBoxesInputTest {
+
+    private void setKeyboard(String data) {
+        InputStream stdin = System.in;
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+    }
 
     @Test
     public void printInput(){
         DotsAndBoxesInput itest=new DotsAndBoxesInput();
+        setKeyboard("Mossa");
         assertEquals(itest.readInput(),"Mossa");
     }
 }
