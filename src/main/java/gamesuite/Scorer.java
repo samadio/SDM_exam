8d6868ff0acfc98172e1be6b2784ee698ebc6a9c
Federico Camerota <fedes770@gmail.com>
2019-12-21 21:58:08 +0100

    Created test environment for Scorer testing

---
+++ src/main/java/gamesuite/Scorer.java
@@ -0,0 +1,4 @@
+package gamesuite;
+
+public class Scorer {
+}
---
+++ src/test/java/gametests/statustest/ScorerTest.java
@@ -0,0 +1,41 @@
+package gametests.statustest;
+
+import gamesuite.Move;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScorerTest {
+
+}
+
+class DummyBoardManager {
+
+    private final Map<Move, Boolean> MOVES_DONE;
+    private final Map<Move, Boolean> MOVES_VALIDITY;
+
+    DummyBoardManager (Map<Move, Boolean> movesDone, Map<Move, Boolean> movesValidity){
+
+        MOVES_DONE = movesDone;
+        MOVES_VALIDITY = movesValidity;
+    }
+
+    public boolean moveDone(Move move){
+
+        Boolean done = MOVES_DONE.get(move);
+
+        if (done == null)
+            throw new InvalidMoveException();
+
+        return done;
+    }
+
+    public boolean isValidMove(Move move){
+
+        Boolean valid = MOVES_VALIDITY.get(move);
+        if (valid == null)
+            throw new InvalidMoveException();
+        return valid;
+    }
+}
