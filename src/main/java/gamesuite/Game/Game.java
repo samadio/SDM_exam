4c8b7e660378c27a35c078ad9d3fd7dd2ea0456c
Simone Amadio <amadiosimone1996@gmail.com>
2019-12-23 21:01:00 +0100

    Tentative Game Structure

--- src/main/java/gamesuite/Game/Game.java
+++ src/main/java/gamesuite/Game/Game.java
@@ -1,29 +1,66 @@
 package gamesuite.Game;
 
 import gamesuite.board.Board;
+import gamesuite.board.BoardManager;
+import gamesuite.move.Move;
+import gamesuite.move.MoveValidator;
+import gamesuite.players.Player;
+import gamesuite.players.PlayersFactory;
 import gamesuite.status.GameScore;
+import iomanagement.InputManager;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Game {
-/*    private Board board;
+    private InputManager imanager;
+    private BoardManager boardManager;
     private MoveValidator validator;
     private GameScore status;
+    //private OutputManager outmanager;
 
-    public Game(Board board, MoveValidator validator,GameStatus status){
-        this.board=board;
+    public Game(Board boardManager, MoveValidator validator,GameScore status,InputManager imanager){
+        this.boardManager=boardManager;
         this.validator=validator;
         this.status=status;
+        this.imanager=imanager;
     }
 
-    public void applyMove(Move currentMove) throws InvalidMoveException {
-        if(validator.isNotValidMove(currentMove)){
-            throw new InvalidMoveException("The move selected (to be printed) is not valid");
+    private void setGrid() {
+        int m, n;
+        Array<Integer> dimensions = imanager.getGrid(); //function that asks for grid dimensions and returns it
+        n = dimensions[0];
+        m = dimensions[1];
+        this.boardManager = new boardManager(n, m);
+    }
+
+    private void setPlayers() {
+        Integer number = imanager.getPlayersNumber(); //how many players are there?
+        ArrayList<Player> players= new ArrayList<Player>(number);
+
+        PlayersFactory playerGenerator= new PlayersFactory();
+        boolean costumize = imanager.customizePlayers(); //do you want to customize Players name? Yes=True
+        if (costumize) {
+            for (Integer i = 0; i < number ; i++) {
+                String name = imanager.getCustomName();
+                if (name.isEmpty()) players[i]=playerGenerator.newPlayer();
+                else players[i]=playerGenerator.newPlayer(name);
+            }
         }
-        board.applyMove(currentMove);
-        status.update(currentMove);
+        //no custom
+        else{
+            for(Integer i=0;i<number;i++) players[i]=playerGenerator.newPlayer();
+        }
     }
 
+
+    public void start(){
+        setGrid();
+        setPlayers();
+    }
+
+
+/*
         public boolean notEnded();
-
-
  */
 }
