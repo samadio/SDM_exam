7748f31576b34c7c75a932f41cbb8229722ba52c
Federico Camerota <fedes770@gmail.com>
2019-12-24 10:35:20 +0100

    Added failing test for score assignment

--- src/main/java/gamesuite/status/GameStatus.java
+++ src/main/java/gamesuite/status/GameStatus.java
@@ -13,12 +13,19 @@ public class GameStatus {
     private final Scorer SCORER;
     private final List<Player> PLAYERS;
     private Integer currentPlayer;
+    private GameScore score;
+
 
     public GameStatus(List<Player> players, BoardManager bManager, MoveValidator mValidator) {
 
         PLAYERS = players;
         SCORER = new Scorer(bManager, mValidator);
         currentPlayer = 0;
+
+        score = new GameScore();
+
+        for (Player p : PLAYERS)
+            score.put(p, 0);
     }
 
     public Player currentPlayer() {
@@ -33,5 +40,10 @@ public class GameStatus {
 
     private Integer nextPlayer() {
         return (currentPlayer + 1) % PLAYERS.size();
+    }
+
+    public GameScore getScore() {
+
+        return score;
     }
 }
--- src/test/java/gametests/statustest/GameStatusTest.java
+++ src/test/java/gametests/statustest/GameStatusTest.java
@@ -5,6 +5,7 @@ import gamesuite.move.Move;
 import gamesuite.move.MoveValidator;
 import gamesuite.players.Player;
 import gamesuite.players.PlayersFactory;
+import gamesuite.status.GameScore;
 import gamesuite.status.GameStatus;
 import org.junit.jupiter.api.Test;
 
@@ -64,5 +65,24 @@ class GameStatusTest {
         gStatus.update(lastMove);
         assertEquals(players.get(0), gStatus.currentPlayer());
 
+    }
+
+    @Test
+    void pointAssigned() {
+
+        Move lastMove = new Move(Move.Which.HORIZONTAL, 0, 0);
+
+        PlayersFactory pFactory = new PlayersFactory();
+        List<Player> players = Arrays.asList(pFactory.newPlayer("Pippo"), pFactory.newPlayer("Pluto"));
+        BoardManager bManager = new DummyBoardManager(ScorerTest.moveMap(lastMove, Arrays.asList(true, false, false, false, true, true, true)));
+        MoveValidator mValidator = new DummyValidator(ScorerTest.moveMap(lastMove, Arrays.asList(true, false, false, false, true, true, true)));
+        GameStatus gStatus = new GameStatus(players, bManager, mValidator);
+
+        GameScore score = gStatus.getScore();
+        assertEquals(0, score.get(players.get(0)));
+        assertEquals(0, score.get(players.get(1)));
+        gStatus.update(lastMove);
+        assertEquals(1, score.get(players.get(0)));
+        assertEquals(0, score.get(players.get(1)));
     }
 }
