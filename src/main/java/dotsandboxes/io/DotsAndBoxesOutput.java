2a82c019683647f26ea66bf727c7a69ce5eb5c51
Simone Amadio <amadiosimone1996@gmail.com>
2019-12-29 18:36:28 +0100

    output print almost done.convertToString missing

--- src/main/java/dotsandboxes/io/DotsAndBoxesOutput.java
+++ src/main/java/dotsandboxes/io/DotsAndBoxesOutput.java
@@ -3,6 +3,7 @@ package dotsandboxes.io;
 import gamesuite.board.AbstractBoard;
 import gamesuite.game.Game;
 import gamesuite.move.InvalidMoveException;
+import gamesuite.move.Move;
 import gamesuite.players.Player;
 import gamesuite.status.GameScore;
 import iomanagement.OutputManager;
@@ -10,6 +11,9 @@ import iomanagement.OutputManager;
 import java.util.Comparator;
 import java.util.Map;
 import java.util.Optional;
+
+import static gamesuite.move.Move.Which.HORIZONTAL;
+import static gamesuite.move.Move.Which.VERTICAL;
 
 public class DotsAndBoxesOutput extends OutputManager {
 
@@ -59,5 +63,34 @@ public class DotsAndBoxesOutput extends OutputManager {
     public void printBoard(AbstractBoard board) {
         Integer rows=board.getRows();
         Integer columns=board.getColumns();
+
+        Move.Which type= HORIZONTAL;
+        Integer i=0; //row u're looking at
+        while(i!=rows-1 && type!=VERTICAL){   //stop condition: first invalid row
+            for(Integer j=0;j<columnsOf(type,columns);j++){
+                Boolean present= board.getElement(type,i,j);
+                String s=convertToString(present,type);
+                outputPrint(s);
+            }
+            if(type==VERTICAL) i+=1;
+            type=other(type);
+            outputPrint("\n");
+        }
     }
+
+    private Integer columnsOf(Move.Which lk,Integer cols){
+        if((lk== HORIZONTAL)) return cols-1;
+        return cols;
+    }
+
+    private Move.Which other(Move.Which lk){
+        if(lk==HORIZONTAL) return VERTICAL;
+        return HORIZONTAL;
+    }
+
+    private String convertToString(Boolean present, Move.Which type) {
+        return null;
+    }
+
+
 }
