0b93eff72343a190c9f0c35f70aeeb03b05c39ed
Simone Amadio <amadiosimone1996@gmail.com>
2020-01-16 12:26:56 +0100

    [REFACTORING]: Integer numbers now cannot be selected as names -Refactoring with functional Intstream -Changed a variable name in order to make it more precise

--- src/main/java/gamesuite/players/PlayersFactory.java
+++ src/main/java/gamesuite/players/PlayersFactory.java
@@ -5,15 +5,15 @@ import java.util.ArrayList;
 public class PlayersFactory {
 
     private Integer numberOfPlayers = 1;
-    private ArrayList<String> usedNames= new ArrayList<>();
+    private ArrayList<String> usedCustomNames= new ArrayList<>();
 
 
     public Player newPlayer(String name) throws NameAlreadyUsedException, ReservedNameException {
-        if(usedNames.contains(name)) throw new NameAlreadyUsedException();
+        if(usedCustomNames.contains(name)) throw new NameAlreadyUsedException();
         boolean isNumeric = name.trim().chars().allMatch( Character::isDigit );
         if(isNumeric) throw new ReservedNameException();
         numberOfPlayers++;
-        usedNames.add(name);
+        usedCustomNames.add(name);
         return new Player(name);
     }
 
