1d7524a982879674b411724ea1dd0afe31738790
Federico Camerota <fedes770@gmail.com>
2020-01-18 14:20:57 +0100

    Performed refactoring on DBButton class

--- src/main/java/dotsandboxes/gui/graphics/DBButton.java
+++ src/main/java/dotsandboxes/gui/graphics/DBButton.java
@@ -3,36 +3,46 @@ package dotsandboxes.gui.graphics;
 import javax.swing.*;
 
 public class DBButton extends JButton {
-    String text;
-    Integer x_position;
-    Integer y_position;
-    Integer width;
-    Integer height;
 
     public DBButton(String t, Integer x, Integer y, Integer w, Integer h ){
-        super();
-        text=t;
-        x_position=x;
-        y_position=y;
-        width=w;
-        height=h;
-        this.setText(text);
-        this.setHorizontalAlignment(JLabel.CENTER);
-        this.setVerticalAlignment(JLabel.CENTER);
-        this.setBounds(x_position, y_position, width, height);
 
+        super();
+
+        setText(t);
+        setSize(w, h);
+        setLocation(x,y);
+
+        setAlignment(JLabel.CENTER);
+
+        this.setBounds(getX(), getY(), getWidth(), getHeight());
+
+        setStyle();
+
+        setTextPosition(SwingConstants.CENTER);
+    }
+
+    public void setDark() {
+        ImageIcon darkIcon = new ImageIcon(new ImageIcon("images/woodTableDark.png").getImage().getScaledInstance(getWidth(), getHeight(), 80));
+        this.setIcon(darkIcon);
+    }
+
+    private void setAlignment(Integer alignment){
+
+        this.setHorizontalAlignment(alignment);
+        this.setVerticalAlignment(alignment);
+    }
+    private void setStyle() {
 
         this.setOpaque(false);
         this.setContentAreaFilled(false);
         this.setBorderPainted(false);
         this.setFocusPainted(false);
-        ImageIcon imageIcon = new ImageIcon(new ImageIcon("images/woodTable.png").getImage().getScaledInstance(w,h, 80));
+        ImageIcon imageIcon = new ImageIcon(new ImageIcon("images/woodTable.png").getImage().getScaledInstance(getWidth(), getHeight(), 80));
         this.setIcon(imageIcon);
-        this.setVerticalTextPosition(SwingConstants.CENTER);
-        this.setHorizontalTextPosition(SwingConstants.CENTER);
     }
-    public void setDark() {
-        ImageIcon darkIcon = new ImageIcon(new ImageIcon("images/woodTableDark.png").getImage().getScaledInstance(width, height, 80));
-        this.setIcon(darkIcon);
+    private void setTextPosition(Integer position) {
+
+        this.setVerticalTextPosition(position);
+        this.setHorizontalTextPosition(position);
     }
 }
