dcd0b56664ac8e07828ebdfb14c78f2a36ef7d98
Andrea Gasparin <andrea.gasparin.87@gmail.com>
2020-01-30 15:39:27 +0100

    merge fatto

---
+++ src/main/java/dotsandboxes/gui/graphics/lists/DBButtonList.java
@@ -0,0 +1,10 @@
+package dotsandboxes.gui.graphics.lists;
+
+import dotsandboxes.gui.graphics.DBButtons.DBButton;
+
+import java.util.LinkedList;
+
+public class DBButtonList extends  LinkedList<DBButton> {
+
+
+}
